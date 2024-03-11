5389e50dc17b5250d6bbfd3819ea1f71f80ca9b5
Varshithvhegde <varshithvh@gmail.com>
2024-03-11 22:19:34 +0530

    Added Opengraph suport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/nav-bar/navbar"
-import logo from "../../public/logo.png"
+import NavBar from "@/components/nav-bar/navbar";
+import logo from "../../public/logo.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Poetique",
   description: "Beautify your poem",
   icons: logo.src,
+  openGraph: {
+    url: "https://poetique.vercel.app/",
+    title: "Poetique",
+    description: "Turn your words into art",
+    siteName: "Poetique",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/Varshithvhegde/jira-jnd/main/BG_simple.png?token=GHSAT0AAAAAACMOA5USTVDAEOLNOITC2FOQZPPGV4A",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -18,9 +29,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta property="og:title" content="Poetique"></meta>
+      <meta property="og:site_name" content="Poetique"></meta>
+      <meta property="og:url" content="https://poetique.vercel.app/"></meta>
+      <meta property="og:description" content="Turn your words into art"></meta>
+      <meta property="og:type" content=""></meta>
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/Varshithvhegde/jira-jnd/main/BG_simple.png?token=GHSAT0AAAAAACMOA5USTVDAEOLNOITC2FOQZPPGV4A"
+      ></meta>
       <link rel="icon" href="/logo.ico" sizes="any" />
       <body className={inter.className}>
-      <NavBar></NavBar>
+        <NavBar></NavBar>
         {children}
       </body>
     </html>
